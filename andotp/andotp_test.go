fee0e2fc59e0e032062a7ad95cf2f4fb6740c512
Rijul Gulati <grijul@protonmail.ch>
2021-06-02 20:42:21 +0530

    Improved test coverage

--- andotp/andotp_test.go
+++ andotp/andotp_test.go
@@ -7,8 +7,9 @@ import (
 )
 
 const (
-	jsonstr  string = "[{\"secret\":\"SOMEAWESOMESECRET\",\"issuer\":\"SOMEAWESOMEISSUER\",\"label\":\"TESTLABEL\",\"digits\":6,\"type\":\"TOTP\",\"algorithm\":\"SHA1\",\"thumbnail\":\"Default\",\"last_used\":1000000000000,\"used_frequency\":0,\"period\":30,\"tags\":[]}]"
-	password string = "testpass"
+	jsonstr         string = "[{\"secret\":\"SOMEAWESOMESECRET\",\"issuer\":\"SOMEAWESOMEISSUER\",\"label\":\"TESTLABEL\",\"digits\":6,\"type\":\"TOTP\",\"algorithm\":\"SHA1\",\"thumbnail\":\"Default\",\"last_used\":1000000000000,\"used_frequency\":0,\"period\":30,\"tags\":[]}]"
+	jsonstrNoSecret string = "[{\"secret\":,\"issuer\":\"SOMEAWESOMEISSUER\",\"label\":\"TESTLABEL\",\"digits\":6,\"type\":\"TOTP\",\"algorithm\":\"SHA1\",\"thumbnail\":\"Default\",\"last_used\":1000000000000,\"used_frequency\":0,\"period\":30,\"tags\":[]}]"
+	password        string = "testpass"
 )
 
 func TestEncryptDecrypt(t *testing.T) {
@@ -25,5 +26,23 @@ func TestEncryptDecrypt(t *testing.T) {
 
 	if string(decryptedtext) != jsonstr {
 		t.Error("Encryption/Decryption failed. Text mismatch")
+	}
+
+	// With wrong password
+
+	encryptedtext = nil
+	decryptedtext = nil
+	err = nil
+
+	encryptedtext, err = andotp.Encrypt([]byte(jsonstr), password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = andotp.Decrypt(encryptedtext, "someotherpass")
+	if err != nil {
+		if err.Error() != "error: cipher: message authentication failed" {
+			t.Error(err)
+		}
 	}
 }

f0d074426dce12bfbf74adb3f51c40d6705d1d22
Rijul Gulati <grijul@protonmail.ch>
2021-06-02 20:42:49 +0530

    minor fix

--- andotp/andotp_test.go
+++ andotp/andotp_test.go
@@ -7,9 +7,8 @@ import (
 )
 
 const (
-	jsonstr         string = "[{\"secret\":\"SOMEAWESOMESECRET\",\"issuer\":\"SOMEAWESOMEISSUER\",\"label\":\"TESTLABEL\",\"digits\":6,\"type\":\"TOTP\",\"algorithm\":\"SHA1\",\"thumbnail\":\"Default\",\"last_used\":1000000000000,\"used_frequency\":0,\"period\":30,\"tags\":[]}]"
-	jsonstrNoSecret string = "[{\"secret\":,\"issuer\":\"SOMEAWESOMEISSUER\",\"label\":\"TESTLABEL\",\"digits\":6,\"type\":\"TOTP\",\"algorithm\":\"SHA1\",\"thumbnail\":\"Default\",\"last_used\":1000000000000,\"used_frequency\":0,\"period\":30,\"tags\":[]}]"
-	password        string = "testpass"
+	jsonstr  string = "[{\"secret\":\"SOMEAWESOMESECRET\",\"issuer\":\"SOMEAWESOMEISSUER\",\"label\":\"TESTLABEL\",\"digits\":6,\"type\":\"TOTP\",\"algorithm\":\"SHA1\",\"thumbnail\":\"Default\",\"last_used\":1000000000000,\"used_frequency\":0,\"period\":30,\"tags\":[]}]"
+	password string = "testpass"
 )
 
 func TestEncryptDecrypt(t *testing.T) {
